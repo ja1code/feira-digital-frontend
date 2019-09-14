400e3e44fc784406ec01a9c63c5b5225292d5a74
Gustavo Freitas <gustavo.hfn.2001@gmail.com>
2019-09-14 11:17:30 -0300

    Search component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
-    <landing/>
+    <search/>
 </template>
 
 <script>
 import landing from './components/landing.vue'
 import loginComerc from "./components/loginComerc.vue"
+import search from "./components/search.vue"
+
 
 export default {
   name: 'app',
   components: {
     landing,
-    loginComerc
+    loginComerc,
+    search
   }
 }
 </script>
---
+++ src/components/search.vue
@@ -0,0 +1,69 @@
+<template>
+<div class="bgSearch">
+    <h1 class="titleSearch">Feira Digital</h1>
+    <p style="margin-left: 3%;"><b>Você buscou por:</b></p>
+    <h2 style="margin-left: 3%;"><b>Mi band 4</b></h2>
+   <div class="container products">
+  <div class="row">
+    <div class="col">
+      <div class="card bg-dark" style="width: 9rem;">
+  <img src="../assets/miBand.webp" class="card-img-top">
+  <div class="card-body">
+    <p class="pSearch">Mi band 4<br>Banca do zé</p>
+    <a href="#" class="pSearch" >COMO CHEGAR</a>
+  </div>
+</div>
+    </div>
+    <div class="col">
+      <div class="card bg-dark" style="width: 9rem;">
+  <img src="../assets/miBand.webp" class="card-img-top">
+  <div class="card-body">
+    <p class="pSearch">Mi band 4<br>Eletrônicos xingling</p>
+
+    <a href="#" class="pSearch">COMO CHEGAR</a>
+  </div>
+</div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      <div class="card bg-dark" style="width: 9rem;">
+  <img src="../assets/miBand.webp" class="card-img-top">
+  <div class="card-body">
+    <p class="pSearch">Mi band 4<br>Banca do zé</p>
+    <a href="#" class="pSearch" >COMO CHEGAR</a>
+  </div>
+</div>
+    </div>
+    <div class="col">
+      <div class="card bg-dark" style="width: 9rem;">
+  <img src="../assets/miBand.webp" class="card-img-top">
+  <div class="card-body">
+    <p class="pSearch">Mi band 4<br>Eletrônicos xingling</p>
+
+    <a href="#" class="pSearch">COMO CHEGAR</a>
+  </div>
+</div>
+    </div>
+  </div>
+</div>    
+</div>
+</template>
+
+<script>
+
+
+export default {
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+@import url("../style/style.css");
+
+</style>
+
